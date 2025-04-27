481c62ae06653a124c532b83a2a7940dcdcd6b38
Valentino Orlandi <valentinox@disroot.org>
2025-04-27 12:03:38 +0000

    Updated lib.rs
    Added the new modules.
    Added `halt()` function.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,29 @@
 
 mod kernel;
 
+pub(crate) mod cpu;
+pub(crate) mod memory;
+pub(crate) mod panic;
+pub(crate) mod traits;
 pub(crate) mod tty;
 
+#[cfg(feature="unit_tests")]
+pub(crate) mod test;
+
+pub(crate) use crate::panic::*;
+pub(crate) use crate::traits::*;
+
+
 #[no_mangle]
-pub extern "C" fn kernel_main() {
+pub extern "C"
+fn kernel_main() {
     kernel::start();
+}
+
+
+pub(crate)
+fn halt() {
+    unsafe {
+        core::arch::asm!("hlt");
+    }
 }
