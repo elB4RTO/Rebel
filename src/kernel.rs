aff6c25edac323f639c0df6f3393de73978ff437
Valentino Orlandi <valentinox@disroot.org>
2025-07-04 11:47:08 +0000

    Updated kernel.rs
    Removed the import of TTY functions in favor of using the fully
    qualified syntax.
    Added function calls to remap the PIC and initialize the PS/2 controller
    and devices.
    Moved down the call to enable interrupts.

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,5 +1,3 @@
-use crate::tty::*;
-
 const KERNEL_NAME : &str = env!("CARGO_PKG_NAME");
 const KERNEL_VERSION : &str = env!("CARGO_PKG_VERSION");
 
@@ -7,28 +5,30 @@ pub(crate)
 fn start() {
     welcome();
 
+    crate::drivers::chips::pic::remap_pic();
     crate::idt::init_idt();
-    crate::idt::enable_interrupts();
 
     crate::memory::init();
     crate::memory::init_kernel_tracing_pages();
-
     #[cfg(feature="unit_tests")]
     {
         crate::memory::tests::run();
         crate::tty::print("\n\n");
     }
-
     crate::memory::book_kernel_allocations_space();
+
+    crate::drivers::chips::ps2::init();
+
+    crate::idt::enable_interrupts();
 
     loop { crate::halt(); }
 }
 
 
 fn welcome() {
-    clear();
-    print(KERNEL_NAME);
-    print(" v");
-    print(KERNEL_VERSION);
-    print("\n");
+    crate::tty::clear();
+    crate::tty::print(KERNEL_NAME);
+    crate::tty::print(" v");
+    crate::tty::print(KERNEL_VERSION);
+    crate::tty::print("\n");
 }
